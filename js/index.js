611284783b0376d036b171748e70cdbae6afd346
deaundrapeek <jingyufanclub@gmail.com>
2017-06-06 17:40:42 -0400

    animate sando

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 window.onload = function (){
 
+  const tl = new TimelineMax();
+  const sando = document.getElementById('sando');
+
+  tl.set(sando, {autoAlpha:1});
+  tl.from(sando, 2.5, { ease: Elastic.easeOut.config(1.5, 0.3), y: -500 });
 }
