3eb98210f78a1bc1be210c3273dc57303d6bdd22
deaundrapeek <jingyufanclub@gmail.com>
2017-06-06 16:08:04 -0400

    set current weather icon for my house

--- js/index.js
+++ js/index.js
@@ -1,15 +1,26 @@
-window.onload = function() {
+window.onload = function (){
 
-// function getWeather() {
-//   const req = new XMLHttpRequest()
-//   req.addEventListener('load');
-//   req.open('GET', 'http://localhost:8080/forecast/5dd2fdd2e6ffb91a72a32b4bdf6538e1/40.65805,-73.9608155')
-//   req.send()
-// }
+  const apiKey = '5dd2fdd2e6ffb91a72a32b4bdf6538e1';
+  const lat = 40.65805;
+  const long = -73.9608155;
+  const url = `http://localhost:8080/forecast/${apiKey}/${lat},${long}`
 
-const skycon = new Skycons({"color": "#fff"});
+  function getWeather(){
+    let req = new XMLHttpRequest();
+    req.open("GET", `http://localhost:8080/forecast/${apiKey}/${lat},${long}`)
+    req.send(null)
+    req.onreadystatechange = function (){
+      if ( (req.readyState == 4) && (req.status == 200) ){
+        let forecast = JSON.parse(req.responseText);
+        let skycon = new Skycons({'color': '#fff'});
+        skycon.add(document.getElementById('weather'), forecast.currently.icon);
+        skycon.play();
+      }
+    }
+  }
 
-skycon.add(document.getElementById("weather"), Skycons.RAIN);
-skycon.play();
+  getWeather();
+
+
 
 }
