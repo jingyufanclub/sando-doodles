37d3a2f091aa5d9afc6c82df849afa2b2fed568a
deaundrapeek <jingyufanclub@gmail.com>
2017-06-07 01:10:12 -0400

    animate

--- js/index.js
+++ js/index.js
@@ -2,9 +2,13 @@ window.onload = function (){
 
   const tl = new TimelineMax();
   const sando = document.getElementById('sando');
-
   const title = document.getElementById('title');
+  const pickle = document.getElementById('pickle');
+  const x = window.innerWidth;
+  const y = window.innerHeight;
 
-  tl.set(sando, {autoAlpha:1});
-  tl.from(sando, 2, { ease: Elastic.easeOut.config(1.5, 0.3), x: -1000 });
+  tl.from(sando, 2, { opacity:0, scale: 0, rotation: 360, ease: SlowMo.ease.config(0.8, 0.5, false) })
+    .to(sando, 0.6, { scale: 0, rotation: 360 }, '+=0.4')
+    .from(title, 0.7, { ease: Bounce.easeOut, top: -y })
+    .fromTo(pickle, 3, { right: -x }, { right: x, repeat: -1, yoyo: true }, '+=0.5');
 }
