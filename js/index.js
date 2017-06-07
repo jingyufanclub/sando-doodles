a7af0de46b5988a8d3447025c8fb1672247d71f1
deaundrapeek <jingyufanclub@gmail.com>
2017-06-06 22:39:26 -0400

    center text

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ window.onload = function (){
   const tl = new TimelineMax();
   const sando = document.getElementById('sando');
 
+  const title = document.getElementById('title');
+
   tl.set(sando, {autoAlpha:1});
-  tl.from(sando, 2.5, { ease: Elastic.easeOut.config(1.5, 0.3), y: -500 });
+  tl.from(sando, 2, { ease: Elastic.easeOut.config(1.5, 0.3), x: -1000 });
 }
