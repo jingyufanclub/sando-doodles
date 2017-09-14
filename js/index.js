4bfe214d37111db56dc51380704fbf7ee842ed00
deaundrapeek <jingyufanclub@gmail.com>
2017-09-13 22:26:53 -0400

    layout using grid

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
+
 window.onload = function (){
 
   const tl = new TimelineMax();
   const main = document.querySelector('main');
-  const sando = document.getElementById('sando');
-  const title = document.getElementById('title');
-  const pickle = document.getElementById('pickle');
+  const logo = document.querySelector('#club-logo');
+  const tagline = document.querySelector('#club-tagline');
+  const mascot = document.querySelector('#mascot');
   const x = window.innerWidth;
   const y = window.innerHeight;
 
   tl.set(main, {autoAlpha:1})
-    .from(sando, 2, { opacity:0, scale: 0, rotation: 360, ease: SlowMo.ease.config(0.8, 0.5, false) })
-    .to(sando, 0.6, { scale: 0, rotation: 360 }, '+=0.5')
-    .from(title, 0.7, { ease: Bounce.easeOut, top: -y })
-    .fromTo(pickle, 3, { right: -x }, { right: x, repeat: -1, yoyo: true }, '+=0.5');
+    .from(logo, 1.6, { opacity:0, scale: 0, rotation: 360, ease: SlowMo.ease.config(0.8, 0.5, false) })
+    .to(logo, 0.6, { scale: 0, rotation: 360 }, '+=0.5')
+    .from(tagline, 0.7, { ease: Bounce.easeOut, y: -y })
+    .fromTo(mascot, 3, { x: -x }, { x: x, repeat: -1, yoyo: true }, '+=0.3');
 }
